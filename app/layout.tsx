845aaed8bd8dbed5e792668d0617dee9344e1fe0
Yashi <yuuto.ron0611@gmail.com>
2025-10-18 15:25:42 +0900

    add:footerの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { HeaderNav } from "@/components/navigation-menu"
+import FooterSection from "@/components/footer"
 
 export default function RootLayout({
   children,
@@ -11,7 +12,7 @@ export default function RootLayout({
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body>
+        <body className="flex min-h-screen flex-col">
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -19,9 +20,10 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <HeaderNav />
-            <main className="mx-auto max-w-screen-xl px-4 py-6">
+            <main className="mx-auto max-w-screen-xl flex-1 px-4 py-6">
               {children}
             </main>
+            <FooterSection className="bg-muted"/>
           </ThemeProvider>
         </body>
       </html>
